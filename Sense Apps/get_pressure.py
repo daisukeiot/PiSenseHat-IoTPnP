ffffdf740a1e3507bb1d9930f0ea8aba3a291504
Berry Tsai <betsai@microsoft.com>
2020-08-19 18:46:38 +0800

    draft verion for default component

--- Sense Apps/get_pressure.py
+++
@@ -1,5 +0,0 @@
-from sense_hat import SenseHat
-sense = SenseHat()
-sense.clear()
-pressure = sense.get_pressure()
-print(pressure)
--- Sense Apps/show_message.py
+++
@@ -1,4 +0,0 @@
-import sys
-from sense_hat import SenseHat
-sense = SenseHat()
-sense.show_message(sys.argv[1])
